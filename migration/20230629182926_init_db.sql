23f5db26972a31d820b12cb0523811ada8c32c65
yauheni <turbanboss193@gmail.com>
2023-06-29 20:50:00 +0300

    fix migrations;

--- migration/20230629182926_init_db.sql
+++ migration/20230629182926_init_db.sql
@@ -1,26 +1,38 @@
 -- +goose Up
 -- +goose StatementBegin
--- Create the users table
+CREATE TABLE IF NOT EXISTS groups (
+    id SERIAL PRIMARY KEY,
+    group_number VARCHAR(255) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     lastname VARCHAR(255) NOT NULL,
     firstname VARCHAR(255) NOT NULL,
     fathersname VARCHAR(255) NOT NULL DEFAULT '',
-    group_number VARCHAR(255) NOT NULL,
-    login VARCHAR(255) NOT NULL DEFAULT ''
-    );
+    group_id INT NOT NULL,
+    login VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    FOREIGN KEY (group_id) REFERENCES groups (id)
+);
 
--- Create the debt table
 CREATE TABLE IF NOT EXISTS debt (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     amount DECIMAL(10, 2) NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users (id)
-    );
+);
+
+
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-drop table if exists users;
-drop table if exists debt;
+ALTER TABLE debt
+DROP CONSTRAINT IF EXISTS debt_user_id_fkey;
+ALTER TABLE users
+DROP CONSTRAINT IF EXISTS users_group_id_fkey;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS debt;
+DROP TABLE IF EXISTS groups;
 -- +goose StatementEnd

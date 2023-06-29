c0e133c74b320a23960ab3d12fa161f8e9350f7b
yauheni <turbanboss193@gmail.com>
2023-06-29 20:12:26 +0300

    add sign up and sign in methods to users transport layer;

---
+++ internal/handler/user.go
@@ -0,0 +1,81 @@
+package handler
+
+import (
+	"encoding/json"
+	"github.com/Zhenya671/golang-test-task/internal/messages"
+	"github.com/Zhenya671/golang-test-task/internal/model"
+	"io"
+	"net/http"
+)
+
+func (h Handler) SignUp(w http.ResponseWriter, r *http.Request) {
+	var input model.User
+
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		h.log.Warn(err)
+		h.handle_errors(w, http.StatusBadRequest, "Bad Request")
+		return
+	}
+
+	token, err := h.user.SignUp(input)
+	if err != nil {
+		h.log.Warn(err)
+		h.handle_errors(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	var tokenJSON model.Token
+	tokenJSON.Token = token
+
+	marshal, err := json.Marshal(tokenJSON)
+	if err != nil {
+		h.log.Warn(err)
+		h.handle_errors(w, http.StatusBadRequest, messages.AppErrorWithMarshalling.Error())
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write(marshal)
+}
+
+func (h Handler) SignIn(w http.ResponseWriter, r *http.Request) {
+	var input model.User
+
+	all, err := io.ReadAll(r.Body)
+	if err != nil {
+		h.log.Warn(err)
+		h.handle_errors(w, http.StatusBadRequest, messages.AppErrorWithMarshalling.Error())
+		return
+	}
+
+	err = json.Unmarshal(all, &input)
+	if err != nil {
+		h.log.Warn(err)
+		h.handle_errors(w, http.StatusBadRequest, "Bad Request")
+		return
+	}
+
+	token, err := h.user.SignIn(input)
+	if err != nil {
+		h.log.Warn(err)
+		h.handle_errors(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	var tokenJSON model.Token
+	tokenJSON.Token = token
+
+	marshal, err := json.Marshal(tokenJSON)
+	if err != nil {
+		h.log.Warn(err)
+		h.handle_errors(w, http.StatusBadRequest, messages.AppErrorWithMarshalling.Error())
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write(marshal)
+}

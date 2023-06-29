d61ce09c1706348dbc036713b44a70e382652581
yauheni <turbanboss193@gmail.com>
2023-06-30 02:27:24 +0300

    fix path name; add solving algo path;

--- internal/api/v1/user.go
+++ internal/api/v1/user.go
@@ -9,9 +9,12 @@ func (api ApiV1) client(r *mux.Router) *mux.Router {
 	r.HandleFunc("/sign-in", api.h.SignIn).Methods(http.MethodPost)
 	r.HandleFunc("/sign-up", api.h.SignUp).Methods(http.MethodPost)
 
-	v1 := r.PathPrefix("").Subrouter()
+	v1 := r.PathPrefix("/user").Subrouter()
 	v1.Use(api.h.Middleware)
-	v1.HandleFunc("/pay-off", api.h.PayOff).Methods(http.MethodPost)
+	v1.HandleFunc("/debt", api.h.PayOff).Methods(http.MethodPut)
+
+	v2 := v1.PathPrefix("/task").Subrouter()
+	v2.HandleFunc("/algo/{AlgoName}", api.h.SolveAlgo).Methods(http.MethodPost)
 
 	return r
 }

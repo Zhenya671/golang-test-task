8cc3ec118a5da372e29e0693ca89d468a00617ec
yauheni <turbanboss193@gmail.com>
2023-06-30 02:27:02 +0300

    add solving algo handler;

--- internal/handler/user.go
+++ internal/handler/user.go
@@ -2,10 +2,14 @@ package handler
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/Zhenya671/golang-test-task/internal/messages"
 	"github.com/Zhenya671/golang-test-task/internal/model"
+	"github.com/gorilla/mux"
 	"io"
+	"log"
 	"net/http"
+	"strings"
 )
 
 func (h Handler) SignUp(w http.ResponseWriter, r *http.Request) {
@@ -103,6 +107,56 @@ func (h Handler) PayOff(w http.ResponseWriter, r *http.Request) {
 	}
 
 	marshal, err := json.Marshal(payOff)
+	if err != nil {
+		h.handle_errors(w, http.StatusBadRequest, messages.AppErrorWithMarshalling.Error())
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(marshal)
+}
+
+func (h Handler) SolveAlgo(w http.ResponseWriter, r *http.Request) {
+	var requestTask model.Task
+
+	userID := r.Header.Get("UserID")
+	if userID == "" {
+		h.handle_errors(w, http.StatusUnauthorized, messages.AppErrorUnauthorized.Error())
+		return
+	}
+
+	algoName := mux.Vars(r)["AlgoName"]
+	if algoName == "" {
+		h.handle_errors(w, http.StatusBadRequest, messages.AppErrorStatusBadRequest.Error())
+		return
+	}
+	algoName = strings.ReplaceAll(algoName, "-", " ")
+	algoName = strings.Title(algoName)
+	algoName = strings.ReplaceAll(algoName, " ", "")
+	fmt.Println(algoName)
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		h.log.Warn("Failed to read request body:", err)
+		h.handle_errors(w, http.StatusBadRequest, messages.AppErrorStatusBadRequest.Error())
+		return
+	}
+
+	err = json.Unmarshal(body, &requestTask)
+	if err != nil {
+		log.Println("Failed to unmarshal request body:", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	result, err := h.user.SolveAlgo(userID, algoName, requestTask)
+	if err != nil {
+		h.log.Warn(err)
+		h.handle_errors(w, http.StatusBadRequest, messages.AppErrorCantSolveTask.Error())
+		return
+	}
+
+	marshal, err := json.Marshal(result)
 	if err != nil {
 		h.handle_errors(w, http.StatusBadRequest, messages.AppErrorWithMarshalling.Error())
 		return

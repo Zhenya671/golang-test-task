f6d8b7d4e3738c4bce5d7791d4c2ec4abe2d87e7
yauheni <turbanboss193@gmail.com>
2023-06-29 22:37:40 +0300

    add field cashback;

--- internal/model/debt.go
+++ internal/model/debt.go
@@ -1,6 +1,7 @@
 package model
 
 type Debt struct {
-	ID     int     `json:"id"`
-	Amount float64 `json:"amount"`
+	ID       int     `json:"id"`
+	Amount   float64 `json:"amount"`
+	CashBack float64 `json:"cash_back"`
 }

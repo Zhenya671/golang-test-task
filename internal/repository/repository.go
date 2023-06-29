feaa10b6358f410cc94e086777d173dc289eae66
yauheni <turbanboss193@gmail.com>
2023-06-29 22:38:16 +0300

    add payoff method for repository layer;

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -10,6 +10,7 @@ import (
 type IUserRepository interface {
 	SignIn(logIn model.User) (model.User, error)
 	SignUp(user model.User) (model.User, error)
+	PayOff(userId string, input model.Debt) (model.Debt, error)
 }
 
 type Repository struct {
@@ -45,7 +46,7 @@ func (r *Repository) SignIn(logIn model.User) (model.User, error) {
 }
 
 func (r *Repository) SignUp(user model.User) (model.User, error) {
-	query := `insert into users(lastname, firstname, fathersname, group_number, login, password) values ($1, $2, $3, (SELECT id FROM groups WHERE group_number = $4), $5, $6) returning id, login`
+	query := `insert into users(lastname, firstname, fathersname, group_id, login, password) values ($1, $2, $3, (SELECT id FROM groups WHERE group_number = $4), $5, $6) returning id, login`
 
 	var savedUser model.User
 	row := r.DB.QueryRow(query, user.LastName, user.FirstName, user.FathersName, user.GroupNumber, user.Login, user.Password)
@@ -57,4 +58,49 @@ func (r *Repository) SignUp(user model.User) (model.User, error) {
 		return savedUser, err
 	}
 	return savedUser, nil
+}
+
+func (r *Repository) PayOff(userId string, input model.Debt) (model.Debt, error) {
+	var debt model.Debt
+
+	tx, err := r.DB.Begin()
+	if err != nil {
+		return debt, err
+	}
+	defer func() {
+		if p := recover(); p != nil {
+			_ = tx.Rollback()
+			panic(p)
+		} else if err != nil {
+			_ = tx.Rollback()
+		} else {
+			err = tx.Commit()
+		}
+	}()
+
+	var userDebt float64
+	err = tx.QueryRow(`
+	SELECT amount 
+	FROM debt
+	WHERE user_id = $1`, userId).Scan(&userDebt)
+	if err != nil {
+		return debt, err
+	}
+
+	amount := userDebt - input.Amount
+	if amount <= 0 {
+		debt.CashBack = amount * -1
+		amount = 0
+	}
+
+	row := tx.QueryRow(`
+	UPDATE debt 
+	SET amount = $1
+	WHERE user_id = $2
+	RETURNING id, amount`, amount, userId)
+	err = row.Scan(&debt.ID, &debt.Amount)
+	if err != nil {
+		return debt, err
+	}
+	return debt, nil
 }
